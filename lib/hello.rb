04e4c95b6b1d78c351d997b1ca816bc81618bce2
Urika Pye <urikapyearies@gmail.com>
2020-01-11 04:26:22 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,9 @@
-def hello_t
+def hello_t(array)
+  i = 0
 
+  while i < array.length
+    i = i + 1
 end
 
 # call your method here!
 
-["Tim", "Tom", "Jim"].each do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
